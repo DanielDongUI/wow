16825f278a9a8a41bad5b4778818920e52bff6cc
DanielDongUI <62261057+DanielDongUI@users.noreply.github.com>
2020-08-17 13:15:03 -0500

    add Profile page with fake data

--- client/components/mainPage/Profile.js
+++ client/components/mainPage/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View , Text, StyleSheet, StatusBar, Image, Buttom, Dimensions} from 'react-native'
+import { View , Text, StyleSheet, StatusBar, Image, Buttom, Dimensions, SafeAreaView, ScrollView} from 'react-native'
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -7,22 +8,233 @@ class Profile extends React.Component {
 
     render(){
         return(
-            <View style={this.props.currentPage==="Profile" ? styles.container : {display:'none'}}>
-                <Text>Profile</Text>
-            </View>
-        )
-    }
+            <SafeAreaView style={this.props.currentPage==="Profile" ? styles.container : {display:'none'}}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ alignSelf: "center" }}>
+                    <View style={styles.profileImage}>
+                        <Image source={require("../../data/userImg/1.jpeg")} style={styles.image} resizeMode="center"></Image>
+                    </View>
+                    <View style={styles.dm}>
+                        <MaterialIcons name="chat" size={18} color="#DFD8C8"></MaterialIcons>
+                    </View>
+                    <View style={styles.active}></View>
+                    <View style={styles.add}>
+                        <Ionicons name="ios-add" size={48} color="#DFD8C8" style={{ marginTop: 6, marginLeft: 2 }}></Ionicons>
+                    </View>
+                </View>
 
+                <View style={styles.infoContainer}>
+                    <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>Zhiping Dong</Text>
+                    
+                </View>
+
+                <View style={styles.statsContainer}>
+                    <View style={styles.statsBox}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>2</Text>
+                        <Text style={[styles.text, styles.subText]}>Posts</Text>
+                    </View>
+                    <View style={[styles.statsBox, { borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1 }]}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>44</Text>
+                        <Text style={[styles.text, styles.subText]}>Followers</Text>
+                    </View>
+                    <View style={styles.statsBox}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>17</Text>
+                        <Text style={[styles.text, styles.subText]}>Following</Text>
+                    </View>
+                </View>
+
+                <View style={{ marginTop: 32 }}>
+                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("../../data/eventImg/1.jpg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("../../data/eventImg/1-2.jpeg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("../../data/eventImg/1-3.jpeg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("../../data/eventImg/1-4.jpeg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("../../data/eventImg/1-5.jpeg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("../../data/eventImg/3.jpeg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                        <View style={styles.mediaImageContainer}>
+                            <Image source={require("../../data/eventImg/3-2.jpg")} style={styles.image} resizeMode="cover"></Image>
+                        </View>
+                    </ScrollView>
+                </View>
+                <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
+                <View style={{ alignItems: "center" }}>
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                Started following <Text style={{ fontWeight: "400" }}>Xinyang Yuan</Text> and <Text style={{ fontWeight: "400" }}>半藏森林</Text>
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                收藏了<Text style={{ fontWeight: "400" }}>有没有人吃夜宵</Text>
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                发布了<Text style={{ fontWeight: "400" }}>一起来植树</Text>
+                            </Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                发布了<Text style={{ fontWeight: "400" }}>一起来看电影</Text>
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
 }
 const deviceWitdth = Dimensions.get('window').width
 const deviceHeight = Dimensions.get('window').height
 
 const styles = StyleSheet.create({
-    container:{
-
+    container: {
+        //flex: 1,
+        backgroundColor: "#FFF",
+        height:deviceHeight*0.83,
+    },
+    text: {
+        fontFamily: "HelveticaNeue",
+        color: "#52575D"
+    },
+    image: {
+        flex: 1,
+        height: undefined,
+        width: undefined
+    },
+    titleBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 24,
+        marginHorizontal: 16
+    },
+    subText: {
+        fontSize: 12,
+        color: "#AEB5BC",
+        textTransform: "uppercase",
+        fontWeight: "500"
+    },
+    profileImage: {
+        marginTop:20,
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        overflow: "hidden"
+    },
+    dm: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: 20,
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    active: {
+        backgroundColor: "#34FFB9",
+        position: "absolute",
+        bottom: 28,
+        left: 10,
+        padding: 4,
+        height: 20,
+        width: 20,
+        borderRadius: 10
+    },
+    add: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    infoContainer: {
+        alignSelf: "center",
+        alignItems: "center",
+        marginTop: 16
+    },
+    statsContainer: {
+        flexDirection: "row",
+        alignSelf: "center",
+        marginTop: 32
+    },
+    statsBox: {
+        alignItems: "center",
+        flex: 1
+    },
+    mediaImageContainer: {
+        width: 180,
+        height: 200,
+        borderRadius: 12,
+        overflow: "hidden",
+        marginHorizontal: 10
+    },
+    mediaCount: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: "50%",
+        marginTop: -50,
+        marginLeft: 30,
+        width: 100,
+        height: 100,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 12,
+        shadowColor: "rgba(0, 0, 0, 0.38)",
+        shadowOffset: { width: 0, height: 10 },
+        shadowRadius: 20,
+        shadowOpacity: 1
+    },
+    recent: {
+        marginLeft: 78,
+        marginTop: 32,
+        marginBottom: 6,
+        fontSize: 10
+    },
+    recentItem: {
+        flexDirection: "row",
+        alignItems: "flex-start",
+        marginBottom: 16
+    },
+    activityIndicator: {
+        backgroundColor: "#CABFAB",
+        padding: 4,
+        height: 12,
+        width: 12,
+        borderRadius: 6,
+        marginTop: 3,
+        marginRight: 20
     }
-}
-)
+});
 const mapStatetoProps = state =>{
     return {
         loginPageStatus : state.statusList.loginPageStatus,
